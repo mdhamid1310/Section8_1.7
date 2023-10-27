018979dc1b250d87e7357cedd42be978fa6ddec1
mdhamid1310 <john@example.com>
2023-10-27 08:57:44 +0530

    Add project files.

---
+++ Section8_1.7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SinglyLibrary;
+
+namespace Section8_1._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                runApp();
+                Console.ReadLine();
+
+            }
+
+        }
+
+        private static void runApp()
+        {
+            SingleLinkedList linkedList = new SingleLinkedList();
+
+            // Adding nodes to the linked list
+            linkedList.AddNode(1);
+            linkedList.AddNode(2);
+            linkedList.AddNode(3);
+            linkedList.AddNode(4);
+
+            Console.WriteLine("Linked List:");
+            linkedList.DisplayList();
+        }
+    }
+}
---
+++ SinglyLibrary/SingleLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLibrary
+{
+    public class SingleLinkedList
+    {
+        public Node Head { get; private set; }
+
+        public SingleLinkedList()
+        {
+            Head = null;
+        }
+
+        public void AddNode(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Node current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public void DisplayList()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
